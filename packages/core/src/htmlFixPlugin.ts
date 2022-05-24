a8c99f2407ee033351ee6dd7e3176dc7fa2fcf2d
yzydeveloper <wqrdw@qq.com>
2022-05-24 11:22:41 +0800

    refactor: 插件导出方式改变，新增HtmlFix修复插件

--- packages/core/src/htmlFixPlugin.ts
+++ packages/core/src/htmlFixPlugin.ts
@@ -2,8 +2,8 @@ import type { Plugin } from 'vite'
 import type { InjectOptions } from './types'
 import { parse } from 'node-html-parser'
 
-const VITE_PLUGIN_NAME = 'vite-plugin-fix-html'
-const MARK = `_${VITE_PLUGIN_NAME}_`
+const VITE_PLUGIN_NAME = 'vite-plugin-html-fix'
+const SPLIT_MARK = `_${VITE_PLUGIN_NAME}_`
 
 export function processTags(tags: InjectOptions['tags']): InjectOptions['tags'] {
     const _tags = tags?.map(tag => {
@@ -14,25 +14,30 @@ export function processTags(tags: InjectOptions['tags']): InjectOptions['tags'] 
                 ...attrs,
                 ...(attrs?.href ? { href: '' } : {}),
                 ...(attrs?.src ? { src: '' } : {}),
-                [MARK]: `${attrs?.src || attrs?.href || ''}_${VITE_PLUGIN_NAME}_${attrs?.src ? 'src' : 'href'}`
+                [VITE_PLUGIN_NAME]: `${attrs?.src || attrs?.href || ''}${SPLIT_MARK}${attrs?.src ? 'src' : 'href'}`
             }
         }
     })
     return _tags ?? []
 }
 
-export function createFixHtmlPlugin(): Plugin {
+export function createPluginHtmlFix(): Plugin {
     return {
         name: VITE_PLUGIN_NAME,
         transformIndexHtml: {
             enforce: 'post',
             transform(html) {
-                console.log(parse(html))
                 const root = parse(html)
-                const nodes = root.querySelectorAll(`[${MARK}]`)
-                nodes.forEach(item => {
-                    const [url, , type] = item.getAttribute(MARK)?.split(MARK) || []
+                const nodes = root.querySelectorAll(`[${VITE_PLUGIN_NAME}]`)
+                nodes.forEach(node => {
+                    const [url, type] = node.getAttribute(VITE_PLUGIN_NAME)?.split(SPLIT_MARK) || []
+                    node.setAttribute(type, url)
                 })
+                const _html = root.toString()
+                return {
+                    html: _html,
+                    tags: []
+                }
             }
         }
     }
