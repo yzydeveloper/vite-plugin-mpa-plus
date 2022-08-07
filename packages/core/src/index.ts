233a997a15e82ad2e28281a82bf87bc51bf18270
yzydeveloper <wqrdw@qq.com>
2022-08-07 16:53:50 +0800

    feat: export type declaration

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,8 @@ import type { Options } from './types'
 import { createMpaPlugin } from './multiPagePlugin'
 import { createHtmlFixPlugin } from './htmlFixPlugin'
 
+export * from './types'
+
 function createPlugin(options: Options): PluginOption {
     return [createMpaPlugin(options), createHtmlFixPlugin(options)]
 }
