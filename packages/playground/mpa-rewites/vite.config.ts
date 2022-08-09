98602d44902f478554db9406f7f17032ec11551c
杨兆雨 <yangzhaoyu@digibird.com.cn>
2022-08-09 10:17:06 +0800

    chore: optimize sample code

--- packages/playground/mpa-rewites/vite.config.ts
+++ packages/playground/mpa-rewites/vite.config.ts
@@ -1,38 +1,37 @@
+import type { Rewrite, Pages } from 'vite-plugin-mpa-plus'
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import mpaPlugin from 'vite-plugin-mpa-plus'
 
-export default defineConfig(() => {
-    const app = ['app1', 'app2']
-    const rewrites = []
-    const pages = app.reduce<Record<string, any>>((_pages, pageName) => {
-        _pages[pageName] = {
-            entry: `src/app/${pageName}/index.ts`,
-            filename: `${pageName}.html`,
-            template: `src/app/${pageName}/index.html`,
-            inject: {
-                data: {
-                    title: 'mpa'
-                }
+const entrys = ['app1', 'app2']
+const rewrites: Rewrite[] = []
+const pages = entrys.reduce<Pages>((result, pageName) => {
+    result[pageName] = {
+        entry: `src/app/${pageName}/index.ts`,
+        filename: `${pageName}.html`,
+        template: `src/app/${pageName}/index.html`,
+        inject: {
+            data: {
+                title: 'mpa'
             }
         }
-        rewrites.push({
-            from: `/view-${pageName}`,
-            to: path.posix.join('/', `/src/app/${pageName}/index.html`)
-        })
-        return _pages
-    }, {})
-
-    return {
-        plugins: [
-            vue(),
-            mpaPlugin({
-                pages,
-                historyApiFallback: {
-                    rewrites
-                }
-            }),
-        ],
     }
-})
+    rewrites.push({
+        from: new RegExp(`^/view-${pageName}$`),
+        to: path.posix.join('/', `/src/app/${pageName}/index.html`)
+    })
+    return result
+}, {})
+
+export default defineConfig(() => ({
+    plugins: [
+        vue(),
+        mpaPlugin({
+            pages,
+            historyApiFallback: {
+                rewrites
+            }
+        }),
+    ],
+}))
